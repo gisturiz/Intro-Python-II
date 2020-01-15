43b15df519efd6d182f8488686b04cc4fa8953fc
Gustavo Isturiz <isturizgustavo@gmail.com>
2020-01-15 18:44:11 -0500

    Game MVP reached

--- src/adv.py
+++ src/adv.py
@@ -1,3 +1,6 @@
+import textwrap
+
+from player import Player
 from room import Room
 
 # Declare all the rooms
@@ -39,6 +42,8 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player('Gustavo', room['outside'])
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +54,38 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while True:
+    current_room = player.room
+    print('\nYou are currently: ' + str(current_room))
+
+    my_wrap = textwrap.TextWrapper(width = 40)
+    room_description = textwrap.shorten(text = player.room.description, width=150)
+    print('\n' + my_wrap.fill(text = room_description))
+
+    print('\nWhere do you want to go? Press `N` for North, `E` for East, `S` for South, `W` for West \nOr press q to quit')
+    ans = input()
+
+    if ans == 'n' or ans == 'N':
+        if not hasattr(current_room, 'n_to'):
+            print("Sorry you cannot take that path, try again.")
+        else:
+            player.room = player.room.n_to
+    elif ans == 'e' or ans == 'E': 
+        if not hasattr(current_room, 'e_to'):
+            print("Sorry you cannot take that path, try again.")  
+        else:
+            player.room = player.room.e_to
+    elif ans == 's' or ans == 'S':
+        if not hasattr(current_room, 's_to'):
+            print("Sorry you cannot take that path, try again.") 
+        else: 
+            player.room = player.room.s_to
+    elif ans == 'w' or ans == 'W':
+        if not hasattr(current_room, 'w_to'):
+           print("Sorry you cannot take that path, try again.")
+        else:
+            player.room = player.room.w_to
+    elif ans == 'q' or ans == 'Q':
+        break
+print("\nThanks for playing!")
